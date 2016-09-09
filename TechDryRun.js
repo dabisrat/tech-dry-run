d7fe13914dea79921331d0ab384c242638cff222
Dawet Bisrat <dabisrat@gmail.com>
2016-09-08 22:07:30 -0400

    git floobits

---
+++ TechDryRun.js
@@ -0,0 +1,24 @@
+//Tech Dry Run
+
+var hrr18 = {
+  shepherd: ap
+  name: 'Jacob Goddard',
+    or l
+  'Wallun Chan': 'Coffee!!'
+
+    ard '
+
+    sethkoch: "",
+
+  niclaflamme: "Nic Laflamme"
+  mitchsmall: "Mitch Small"
+
+  mikeHughes: "Mike Hughes",
+  jonDeng: "jon Deng",
+  cambaughn: 'Cameron Baug',
+  heshamabdalla: " Hesham Abdalla"
+  dabisrat: "Dawet Bisrat"
+  
+
+
+};
